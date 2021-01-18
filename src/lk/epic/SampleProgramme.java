39e412b6f5d0cadd1be3ef32ce3682dfcd12c0df
heshan_c <heshanamila94@gmail.com>
2021-01-18 16:34:16 +0530

    a

---
+++ src/lk/epic/SampleProgramme.java
@@ -0,0 +1,15 @@
+package lk.epic;
+
+public class SampleProgramme {
+    int id;
+    String firstName;
+    String lastName;
+
+    public SampleProgramme(int id,String firstName,String lastName){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+
+
+    }
+}
--- src/lk/epic/javaBasics.java
+++ src/lk/epic/javaBasics.java
@@ -3,5 +3,8 @@ package lk.epic;
 public class javaBasics {
     public static void main(String[] args) {
         System.out.println("javaBasics");
+        SampleProgramme s1 = new SampleProgramme(2,"Hershan","Chandrasena");
+        System.out.println(s1.lastName+s1.firstName+s1.id);
+
     }
 }
